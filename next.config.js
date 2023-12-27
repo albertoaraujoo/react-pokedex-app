a6efea0d87c69df7477e1931f007d553e5c71e61
Alberto Araújo <alberto.araujoo@hotmail.com>
2023-12-26 21:29:38 -0300

    fix: 03

--- next.config.js
+++ next.config.js
@@ -2,12 +2,3 @@
 const nextConfig = {};
 
 module.exports = nextConfig;
-
-const withTM = require("next-transpile-modules")(["@"]);
-
-module.exports = withTM({
-  webpack: (config) => {
-    config.resolve.alias["@"] = `${__dirname}/src`;
-    return config;
-  },
-});
